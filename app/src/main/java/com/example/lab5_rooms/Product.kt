23ae060c09efcda49dacc9b49b27d9ca84d4fa75
Trebulkins <trebinivan2005@gmail.com>
2025-11-10 11:57:28 +0700

    stable ver

--- app/src/main/java/com/example/lab5_rooms/Product.kt
+++ app/src/main/java/com/example/lab5_rooms/Product.kt
@@ -9,9 +9,12 @@ class Product {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "productId")
     var id: Int = 0
+
     @ColumnInfo(name = "productName")
     var productName: String = ""
+
     var quantity: Int = 0
+
     constructor()
     constructor(productname: String, quantity: Int) {
         this.productName = productname
